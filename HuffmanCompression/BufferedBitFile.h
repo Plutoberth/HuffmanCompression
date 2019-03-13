6d6dfbbfae3bab44b933067a185f9ea3cf593131
Nir <nir61202@gmail.com>
2019-03-13 21:48:43 +0200

    Change data type

--- HuffmanCompression/BufferedBitFile.h
+++ HuffmanCompression/BufferedBitFile.h
@@ -12,14 +12,10 @@ using std::string;
 
 typedef uint8_t byte;
 
-/*I elected to use a deque of bytes for my bit array. This was chosen because the efficiency of
-/*the container doesn't really matter in this project, and in most implementations of bit writing.
-/*Here, the maximum number of bits that we'll need to store is not very high, at most a couple of kilobytes.
-/*This is excusable because bitsets are much, much slower and do not allow deletions from the beginning and the end,
-/*or easy extractions. I preferred the time and usage efficiency over the space efficiency.
-/*I could have used the specialized vector<bool> that stores bools as bits, but it would've been slower and a deque
-/* is better for this purpose as it allows deletions both from the beginning and the end and that's useful in bit buffering.*/
-typedef std::deque<byte> BitArray;
+/*TODO: Check whether a vector<bool> or a vector<byte> are faster
+/*This might seem obvious, but vector<bool> has a specialization that makes it store each bool as a single bit.
+/*It makes it much more space efficient, but I need to check if the performance decrease isn't significant.*/
+typedef std::vector<byte> BitArray;
 
 //This buffered bit file class will accept arrays of bytes, with each element as a single bit.
 //It'll write the bits to file when it reaches the threshold in bufferSize.
