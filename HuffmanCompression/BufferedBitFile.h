f70a732e79f286b47196fd0c323456336e13235f
Nir <nir61202@gmail.com>
2019-03-13 14:38:56 +0200

    add an explanation for the data type

--- HuffmanCompression/BufferedBitFile.h
+++ HuffmanCompression/BufferedBitFile.h
@@ -10,7 +10,13 @@ using std::string;
 
 typedef uint8_t byte;
 
-//Because
+/*I elected to use a deque of bytes for my bit array. This was chosen because the efficiency of
+/*the container doesn't really matter in this project, and in most implementations of bit writing.
+/*Here, the maximum number of bits that we'll need to store is not very high, at most a couple of kilobytes.
+/*This is excusable because bitsets are much, much slower and do not allow deletions from the beginning and the end,
+/*or easy extractions. I preferred the time and usage efficiency over the space efficiency.
+/*I could have used the specialized vector<bool> that stores bools as bits, but it would've been slower and a deque
+/* is better for this purpose as it allows deletions both from the beginning and the end and that's useful in bit buffering.*/
 typedef std::deque<byte> BitArray;
 
 class BufferedBitFile
